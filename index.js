6e9a2ad6ab9a6bf43addad7385fbbcd8ee815693
Sasa Milasinovic <sasa.milasinovic@gmail.com>
2020-11-20 14:50:00 +0100

    fix paths

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 
-import TiptapEditor from 'src/components/TiptapEditor'
-export * from 'src/extensions/index'
-export * from 'src/components/index'
+import TiptapEditor from './src/components/TiptapEditor'
+export * from './src/extensions/index'
+export * from './src/components/index'
 
 export {
   TiptapEditor,
