4e152f2ad10c6e0dd0a02f4b934d15e54a2ee853
Sasa Milasinovic <sasa.milasinovic@gmail.com>
2020-11-20 14:47:17 +0100

    move index out of src

---
+++ index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import TiptapEditor from 'src/components/TiptapEditor'
+export * from 'src/extensions/index'
+export * from 'src/components/index'
+
+export {
+  TiptapEditor,
+}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-import TiptapEditor from './components/TiptapEditor'
-export * from './extensions/index'
-export * from './components/index'
-
-export {
-  TiptapEditor,
-}
